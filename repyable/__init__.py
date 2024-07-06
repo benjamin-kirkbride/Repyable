f11efb642ffec984b1e79b114e3e43e3c8aecb3f
Benjamin Kirkbride <benjaminkirkbride@gmail.com>
2024-07-05 23:23:50 -0400

    .

--- repyable/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Repyable: A reliable communication protocol implementation over UDP.
-
-This package provides tools for implementing reliable communication
-over unreliable transport layers, handling packet sequencing,
-acknowledgments, fragmentation, and reassembly.
-"""
