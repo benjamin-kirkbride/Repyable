2da6d143c80a42b4ed5500d297932067e468db75
Benjamin Kirkbride <benjaminkirkbride@gmail.com>
2024-07-06 12:01:46 -0400

    Added type annotations to the RealWorldSocket class.

--- tests/real_world_socket.py
+++ tests/real_world_socket.py
@@ -13,17 +13,17 @@ class RealWorldSocket:
         - Bandwidth limitations
     """
 
-    def __init__(self):
-        self._socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    def __init__(self) -> None:
+        self._socket: socket.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-    def connect(self, host, port):
+    def connect(self, host: str, port: int) -> None:
         self._socket.connect((host, port))
 
-    def send(self, data):
-        self._socket.send(data)
+    def send(self, data: bytes) -> int:
+        return self._socket.send(data)
 
-    def receive(self, buffer_size):
+    def receive(self, buffer_size: int) -> bytes:
         return self._socket.recv(buffer_size)
 
-    def close(self):
+    def close(self) -> None:
         self._socket.close()
