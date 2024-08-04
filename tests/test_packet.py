01b34dca557381dc079ca0757467b1f8e3e2f9c0
Benjamin Kirkbride <benjaminkirkbride@gmail.com>
2024-08-04 17:23:08 -0400

    .

--- tests/test_packet.py
+++ tests/test_packet.py
@@ -1,11 +1,10 @@
-from hypothesis import example, given
+from hypothesis import given
 from hypothesis import strategies as st
 
 from repyable import packet
 
 
 @given(payload=st.binary(), packet_type=st.sampled_from(packet.Type))
-@example(payload=b"")
 def test_packet_round_trip(payload: bytes, packet_type: packet.Type) -> None:
     """Test packet round trip."""
     packet_ = packet.Packet.create(packet_type, payload)
